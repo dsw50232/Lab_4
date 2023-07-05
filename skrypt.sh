ebb9fc28d0b23a988c9dbe6a91ffb04f725282e2
Szymon <dsw50232@student.dsw.edu.pl>
2023-07-05 22:41:12 +0200

    dodany skrypt dla logs x

--- skrypt.sh
+++ skrypt.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
- 
+
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiejsza data: $(date +%Y-%m-%d)"
 elif [[ "$1" == "--logs" ]]; then
-    for ((i=1; i<=100; i++)); do
-        echo "Nazwa pliku: log$i.txt"
-        echo "Nazwa skryptu: $0"
-        echo "Data utworzenia: $(date +%Y-%m-%d)" > log$i.txt
-    done
-    echo "Utworzono 100 plików logx.txt"
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        liczba_plikow=$2
+        for ((i=1; i<=liczba_plikow; i++)); do
+            echo "Nazwa pliku: log$i.txt"
+            echo "Nazwa skryptu: $0"
+            echo "Data utworzenia: $(date +%Y-%m-%d)" > log$i.txt
+        done
+        echo "Utworzono $liczba_plikow plików logx.txt"
+    else
+        echo "Nieprawidłowy argument. Podaj liczbę plików."
+    fi
 else
     echo "Nieprawidłowa flaga. Dostępne flagi: --date, --logs"
 fi
