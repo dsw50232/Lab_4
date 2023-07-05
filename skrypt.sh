e49ea99b582a199cc4eb28d27fccd970dfa2f6e1
Szymon <dsw50232@student.dsw.edu.pl>
2023-07-05 20:49:53 +0200

    dodanie skryptu logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,13 @@
 
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+elif [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++)); do
+        echo "Nazwa pliku: log$i.txt"
+        echo "Nazwa skryptu: $0"
+        echo "Data utworzenia: $(date +%Y-%m-%d)" > log$i.txt
+    done
+    echo "Utworzono 100 plików logx.txt"
 else
-    echo "Nieprawidłowa flaga. Dostępne flagi: --date"
+    echo "Nieprawidłowa flaga. Dostępne flagi: --date, --logs"
 fi
