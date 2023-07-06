0c89b83643899aaf0cd18cb957eef68a5c09a6ce
Szymon <dsw50232@student.dsw.edu.pl>
2023-07-06 17:58:53 +0200

    dodanie skryptu dla error

--- skrypt.sh
+++ skrypt.sh
@@ -19,14 +19,26 @@ elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
     echo "  --date, -d      : Wyświetla dzisiejszą datę."
     echo "  --logs N, -l N  : Tworzy N plików logx.txt z informacjami o nazwie pliku, nazwie skryptu i dacie utworzenia."
     echo "  --help, -h      : Wyświetla wszystkie dostępne opcje skryptu."
-elif [[ "$1" == "--init" ]]; then
-    mkdir repo_clon
-    git clone https://github.com/dsw50232/Lab_4.git repo_clon/
-    echo "Repozytorium zostało sklonowane do katalogu 'repo_clon'."
-    echo "Ustawiam ścieżkę w zmiennej środowiskowej PATH."
-    echo "export PATH=\"\$PATH:$(pwd)/repo_clon\"" >> ~/.bashrc
-    echo "Ścieżka została dodana do zmiennej środowiskowej PATH."
-    source ~/.bashrc
+elif [[ "$1" == "--error" ]] || [[ "$1" == "-e" ]]; then
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        liczba_plikow=${2:-100}
+        for ((i=1; i<=liczba_plikow; i++)); do
+            echo "Nazwa pliku: error$i.txt"
+            echo "Nazwa skryptu: $0"
+            echo "Data utworzenia: $(date +%Y-%m-%d)" > error$i.txt
+        done
+        echo "Utworzono $liczba_plikow plików errorx.txt"
+    elif [[ -z "$2" ]]; then
+        liczba_plikow=100
+        for ((i=1; i<=liczba_plikow; i++)); do
+            echo "Nazwa pliku: error$i.txt"
+            echo "Nazwa skryptu: $0"
+            echo "Data utworzenia: $(date +%Y-%m-%d)" > error$i.txt
+        done
+        echo "Utworzono $liczba_plikow plików errorx.txt"
+    else
+        echo "Nieprawidłowy argument. Podaj liczbę plików."
+    fi
 else
-    echo "Nieprawidłowa flaga. Dostępne flagi: --date, -d, --logs, -l, --init, --help, -h"
+    echo "Nieprawidłowa flaga. Dostępne flagi: --date, -d, --logs, -l, --init, --help, -h, -error, -e"
 fi
